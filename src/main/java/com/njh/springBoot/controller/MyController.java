a515adfccc6a20741f836bf1cc8609952f38970b
Noah <Noah@DESKTOP-CG9UJN6.attlocal.net>
2016-11-30 23:49:16 -0500

    no js still

--- src/main/java/com/njh/springBoot/controller/MyController.java
+++ src/main/java/com/njh/springBoot/controller/MyController.java
@@ -1,12 +1,22 @@
-package com.njh.springBoot;
+package com.njh.springBoot.controller;
 
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.njh.springBoot.Student;
+import com.njh.springBoot.service.StudentService;
+
 @Controller
 public class MyController {
+	
+	
+	@Autowired
+	private StudentService studentService;
 	
 	@RequestMapping("/sup")
 	public String sup(@RequestParam(value="name", required=false, defaultValue="bitches")String name,
@@ -16,7 +26,15 @@ public class MyController {
 	}
 	
 	@RequestMapping("/")
-	public String index(){
+	public String index(Model model){
+		HashMap<Long, Student> students = studentService.getAllStudents();
+		model.addAttribute("students", students);
+		
 		return "index";
+	}
+	
+	public String addStudent(String name, String subject){
+		return null;
+		
 	}
 }
--- src/main/java/com/njh/springBoot/service/StudentService.java
+++ src/main/java/com/njh/springBoot/service/StudentService.java
@@ -1,4 +1,4 @@
-package com.njh.springBoot;
+package com.njh.springBoot.service;
 
 import java.util.HashMap;
 
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.njh.springBoot.Application;
+import com.njh.springBoot.Student;
 
 
 @RestController
