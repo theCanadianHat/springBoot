69ddf24469c618b09ee0b91a81b6afbdd5f84979
Noah <Noah@DESKTOP-CG9UJN6.attlocal.net>
2016-12-01 20:00:16 -0500

    wired up the add student button. still not 100%

--- src/main/java/com/njh/springBoot/controller/MyController.java
+++ src/main/java/com/njh/springBoot/controller/MyController.java
@@ -2,9 +2,13 @@ package com.njh.springBoot.controller;
 
 import java.util.HashMap;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -33,8 +37,10 @@ public class MyController {
 		return "index";
 	}
 	
-	public String addStudent(String name, String subject){
-		return null;
-		
+	@RequestMapping("/addStudent")
+	public void addStudent(@RequestParam(value="name") String name, @RequestParam(value="subject") String subject, Model model,
+			HttpServletRequest request, HttpServletResponse response){
+		studentService.addStudent(name, subject);
+		this.index(model);
 	}
 }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -11,8 +11,19 @@ function addStudent(){
 	if($studentName.val() == "" || $studentSubject.val() == ""){
 		$validation.show();
 		return;
+	}else{
+		var newName = $studentName.val();
+		var newSubject = $studentSubject.val();
+		var newURL = window.location.protocol + "//" + window.location.host;
+		
+		$.ajax({
+			method: "GET",
+			url: newURL + "/addStudent",
+			data: {name: newName, subject: newSubject},
+			success: function(){
+				window.location.reload(true);
+			}
+		});
 	}
-	
-	
 	
 }
